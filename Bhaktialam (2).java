128ee81335f3b34452c0f3ac37a980d88cf0a4cd
quinneirn <112993646+quinneirn@users.noreply.github.com>
2022-09-08 23:09:30 +0700

    Add files via upload

---
+++ Bhaktialam (2).java
@@ -0,0 +1,131 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET
+ */
+public class cottagebhaktialam {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("BHAKTI ALAM COTTAGE");
+        System.out.println("Cottage : Duku, jeruk, alpukat, jambu air, durian, belimbing, mannga, kedondong, Barrack ");
+        System.out.println("Silahkan pilih cottage : ");
+        String cottage = sc.nextLine();
+        System.out.print("hari (Weekend/Weekday/Holiday) : ");
+        String hari = sc.nextLine();
+                int room = 0;
+                int harga = 0;
+        
+        if (cottage.equalsIgnoreCase("Duku")) {
+            room = 2;
+        if (hari.equalsIgnoreCase("Weekday"))
+            harga = 915000;
+        else if (hari.equalsIgnoreCase("Weekend"))
+            harga = 1025000;
+        else if (hari.equalsIgnoreCase("Holiday"))
+            harga = 1225000; }
+        
+        else if (cottage.equalsIgnoreCase ("Jeruk")) {
+            room = 2;}
+            if (hari.equalsIgnoreCase("Weekday")){
+                harga = 915000;}
+            else if (hari.equalsIgnoreCase("Weekend")) {
+            harga = 1025000;}
+            else if (hari.equalsIgnoreCase("Holiday")) {
+            harga = 1025000;}
+            
+         else if (cottage.equalsIgnoreCase ("Alpukat")) {
+               room = 1;}
+            if (hari.equalsIgnoreCase("Weekday")){
+                harga = 575000;}
+            else if (hari.equalsIgnoreCase("Weekend")) {
+            harga = 695000;}
+            else if (hari.equalsIgnoreCase("Holiday")) {
+            harga = 895000;}
+         
+        else if (cottage.equalsIgnoreCase ("Jambu air")) {
+            room = 1;}
+            if (hari.equalsIgnoreCase("Weekday")){
+                harga = 575000;}
+            else if (hari.equalsIgnoreCase("Weekend")) {
+            harga = 695000;}
+            else if (hari.equalsIgnoreCase("Holiday")) {
+            harga = 895000;}
+           
+        else if (cottage.equalsIgnoreCase ("Durian")) {
+            room = 2;}
+            if (hari.equalsIgnoreCase("Weekday")){
+                harga = 595000;}
+            else if (hari.equalsIgnoreCase("Weekend")) {
+            harga = 715000;}
+            else if (hari.equalsIgnoreCase("Holiday")) {
+            harga = 915000;}
+            
+        else if (cottage.equalsIgnoreCase ("Melon")) {
+            room = 2;}
+            if (hari.equalsIgnoreCase("Weekday")){
+                harga = 595000;}
+            else if (hari.equalsIgnoreCase("Weekend")) {
+            harga = 715000;}
+            else if (hari.equalsIgnoreCase("Holiday")) {
+            harga = 915000;}
+            
+        else if (cottage.equalsIgnoreCase ("Belimbing")) {
+            room = 2; }
+            if (hari.equalsIgnoreCase("Weekday")){
+                harga = 495000;}
+            else if (hari.equalsIgnoreCase("Weekend")) {
+            harga = 575000;}
+            else if (hari.equalsIgnoreCase("Holiday")) {
+            harga = 755000;}
+    
+        else if (cottage.equalsIgnoreCase ("Mangga")) {
+            room = 2;}
+            if (hari.equalsIgnoreCase("Weekday")){
+                harga = 495000;}
+            else if (hari.equalsIgnoreCase("Weekend")) {
+            harga = 575000;}
+            else if (hari.equalsIgnoreCase("Holiday")) {
+            harga = 755000;}
+            
+        else if (cottage.equalsIgnoreCase ("Belimbing")) {
+            room = 2;}
+            if (hari.equalsIgnoreCase("Weekday")){
+                harga = 495000;}
+            else if (hari.equalsIgnoreCase("Weekend")) {
+            harga = 575000;}
+            else if (hari.equalsIgnoreCase("Holiday")) {
+            harga = 755000;}
+            
+        else if (cottage.equalsIgnoreCase ("Kedondong")) {
+            room = 2; }
+            if (hari.equalsIgnoreCase("Weekday")){
+                harga = 495000;}
+            else if (hari.equalsIgnoreCase("Weekend")) {
+            harga = 575000;}
+            else if (hari.equalsIgnoreCase("Holiday")) {
+            harga = 755000;}   
+            
+            else if (cottage.equalsIgnoreCase ("Barrack")) {
+                if (hari.equalsIgnoreCase("Weekend"))
+                    harga = 25000;
+                else if (hari.equalsIgnoreCase("Weekday")) {
+                harga = 25000;}
+                else if (hari.equalsIgnoreCase("Holiday")) {
+                harga = 35000;}
+
+            }
+            
+        System.out.println("cottage : " +cottage+ "\nroom : "+room+ "\nhari : " +hari+ "\nharga :" +harga); 
+  
+    }
+        
+        }
